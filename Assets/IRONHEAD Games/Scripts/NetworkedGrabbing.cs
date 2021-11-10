62c29e2347313a94368a95f3e31e87b55a28faa9
YoussefSS <joesamir98@gmail.com>
2021-11-10 20:24:19 +0200

    33. Networked Grabbing Part 3: Remote Procedure Calls- RPCs * Grabbing is now synched, + fixed multiple problems with photonView

--- Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
+++ Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
@@ -8,6 +8,10 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
 {
     PhotonView m_photonView;
 
+    Rigidbody rb;
+
+    bool isBeingHeld = false;
+
     private void Awake()
     {
         m_photonView = GetComponent<PhotonView>();
@@ -15,13 +19,23 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isBeingHeld) // Object is being grabbed
+        {
+            rb.isKinematic = true;
+            gameObject.layer = 11; // InHand layer, we don't want others to grab it
+
+        }
+        else
+        {
+            rb.isKinematic = false;
+            gameObject.layer = 9; // Interactable layer, now others can grab it
+        }
     }
 
     private void TransferOwnership()
@@ -32,16 +46,34 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
     public void OnSelectEntered()
     {
         Debug.Log("Grabbed");
-        TransferOwnership();
+        m_photonView.RPC("StartNetworkGrabbing", RpcTarget.AllBuffered);
+
+        if(m_photonView.Owner == PhotonNetwork.LocalPlayer)
+        {
+            Debug.Log("We don't request ownership because it is already mine");
+        }
+        else
+        {
+            TransferOwnership();
+        }
     }
 
     public void OnSelectExited()
     {
         Debug.Log("Released");
+        m_photonView.RPC("StopNetworkGrabbing", RpcTarget.AllBuffered);
     }
 
     public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
     {
+        // If there are many of the same object, there may be a problem as OnOwnershipRequest callback will be called for all of them
+        // This method should be called only for hte grabbed object 
+        if(targetView != m_photonView)
+        {
+            return;
+        }
+
+
         Debug.Log("Ownership requested for: " + targetView.name + " from " + requestingPlayer.NickName);
 
         // Now we will transfer ownership
@@ -57,5 +89,18 @@ public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbac
     public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
     {
         
+    }
+
+    
+    [PunRPC]
+    public void StartNetworkGrabbing()
+    {
+        isBeingHeld = true;
+    }
+
+    [PunRPC]
+    public void StopNetworkGrabbing()
+    {
+        isBeingHeld = false;
     }
 }

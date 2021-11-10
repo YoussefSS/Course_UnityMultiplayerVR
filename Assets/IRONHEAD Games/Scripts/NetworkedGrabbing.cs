6cb9245a7ccbe0b1253d328de016b1c4588a47b3
YoussefSS <joesamir98@gmail.com>
2021-11-10 19:53:45 +0200

    32. Networked Grabbing Part 2: Ownership Transfer * The nerfgun is now synched, as we handled ownership transfer, however there is an issue where the object is moving erratically. This is because when we look at the nerfgun in the unity editor, we see that the gravity is enabled on it.

--- Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
+++ Assets/IRONHEAD Games/Scripts/NetworkedGrabbing.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class NetworkedGrabbing : MonoBehaviour
+public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbacks // Contains callbacks for pun ownership
 {
+    PhotonView m_photonView;
+
+    private void Awake()
+    {
+        m_photonView = GetComponent<PhotonView>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +24,38 @@ public class NetworkedGrabbing : MonoBehaviour
         
     }
 
+    private void TransferOwnership()
+    {
+        m_photonView.RequestOwnership(); // This is called after we try to grab, ownership is now requested. OnOwnershipRequest callback should be called 
+    }
+
     public void OnSelectEntered()
     {
         Debug.Log("Grabbed");
+        TransferOwnership();
     }
 
     public void OnSelectExited()
     {
         Debug.Log("Released");
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        Debug.Log("Ownership requested for: " + targetView.name + " from " + requestingPlayer.NickName);
+
+        // Now we will transfer ownership
+        m_photonView.TransferOwnership(requestingPlayer); // Once done, the OnOwnershipTransfered callback should be called
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        Debug.Log("OnOwnership Transferred to " + targetView.name + "from " + previousOwner.NickName);
+    }
+
+    // Fails for objects with "Takeover" Ownership transfer setting
+    public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
+    {
+        
     }
 }

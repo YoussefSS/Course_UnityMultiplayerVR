a9ef427e748dd4f1aa6b3bac4454d38d6f9fb6c1
YoussefSS <joesamir98@gmail.com>
2021-11-12 01:30:19 +0200

    34. Local Avatar Selection System

--- Assets/IRONHEAD Games/Scripts/MultiplayerVRConstants.cs
+++ Assets/IRONHEAD Games/Scripts/MultiplayerVRConstants.cs
@@ -7,4 +7,6 @@ public class MultiplayerVRConstants : MonoBehaviour
     public const string MAP_TYPE_KEY = "map";
     public const string MAP_TYPE_VALUE_SCHOOL = "school";
     public const string MAP_TYPE_VALUE_OUTDOOR = "outdoor";
+
+    public const string AVATAR_SELECTION_NUMBER = "Avatar_Selection_Number";
 }
